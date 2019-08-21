7a3aae4d6b9c84168b3fce16a6ba9bead68ce9e8
Marc Wright <marc.wright@gmail.com>
2019-08-21 20:47:14 +0300

    Push solutions

---
+++ assignments_solution/04-scroll-to/js/main.js
@@ -0,0 +1,11 @@
+// ############################################################
+// ######################## Applied JS ########################
+// ############################################################
+
+// ScrollTo in action
+// Notice the information in the scrollTo method is greatly simplified
+// All you need to supply is the scrollTo target and the duration in milliseconds (1000ms = 1s)
+$('#take-me-home, #take-me-home-2, #take-me-home-3').click(function () {
+  console.log("clicked")
+  $.scrollTo('.big-text', 600);
+});
