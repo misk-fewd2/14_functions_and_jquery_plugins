a4b5754372595bd633858101807f836371205514
Marc Wright <marc.wright@gmail.com>
2019-08-21 14:55:37 +0300

    New push

---
+++ assignments/01-codealong/js/main.js
@@ -0,0 +1,8 @@
+// ############################################################
+// ######################## Applied JS ########################
+// ############################################################
+
+// Using a Flickity carousel with jQuery
+// Initialization for carousel just attaches like any other jQuery method
+// It works very similar to how you would define multiple CSS property/value pairs with the .css() method
+// Check out the JSON notation for the options
---
+++ assignments/04-scroll-to/js/main.js
@@ -0,0 +1,8 @@
+// ############################################################
+// ######################## Applied JS ########################
+// ############################################################
+
+// ScrollTo in action
+// Notice the information in the scrollTo method is greatly simplified
+// All you need to supply is the scrollTo target and the duration in milliseconds (1000ms = 1s)
+
